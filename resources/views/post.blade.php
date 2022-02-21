33f080c7c580cccbe56028ff230d18b2a7e21ba5
kickinbrain <kickinbrain@gmail.com>
2022-02-21 08:45:18 +0300

    completed episode 7 - make a route and link to it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./app.css">
+    <title>My First Post</title>
+</head>
+<body>
+    <article>
+        <h1>
+            <a href="/post">My First Post</a>
+        </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consequuntur doloremque incidunt ipsam magni nam nemo, nesciunt omnis provident soluta sunt suscipit, vitae voluptatum. Aut cupiditate debitis incidunt laborum nobis obcaecati pariatur possimus tenetur. Aliquam assumenda atque deleniti eos eum expedita id inventore ipsa iste laboriosam libero magni molestias omnis, quia quis quod quos reiciendis sit sunt unde veniam veritatis voluptatem. At fugiat hic itaque magni modi, possimus quas quibusdam. Dolores eius enim fugiat fugit id ipsam sed suscipit ut! Aliquid animi atque aut corporis dicta dignissimos dolorem eius eligendi illo laudantium natus nihil placeat quibusdam quis, quo reprehenderit tempora.
+        </p>
+    </article>
+    <a href="/">Go Back</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./app.css">
+    <title>My blog</title>
+</head>
+<body>
+    <article>
+        <h1>
+            <a href="/post">My First Post</a>
+        </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consequuntur doloremque incidunt ipsam magni nam nemo, nesciunt omnis provident soluta sunt suscipit, vitae voluptatum. Aut cupiditate debitis incidunt laborum nobis obcaecati pariatur possimus tenetur. Aliquam assumenda atque deleniti eos eum expedita id inventore ipsa iste laboriosam libero magni molestias omnis, quia quis quod quos reiciendis sit sunt unde veniam veritatis voluptatem. At fugiat hic itaque magni modi, possimus quas quibusdam. Dolores eius enim fugiat fugit id ipsam sed suscipit ut! Aliquid animi atque aut corporis dicta dignissimos dolorem eius eligendi illo laudantium natus nihil placeat quibusdam quis, quo reprehenderit tempora.
+        </p>
+    </article>
+    <article>
+        <h1>
+            <a href="/post">
+                My Second Post
+            </a>
+        </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consequuntur doloremque incidunt ipsam magni nam nemo, nesciunt omnis provident soluta sunt suscipit, vitae voluptatum. Aut cupiditate debitis incidunt laborum nobis obcaecati pariatur possimus tenetur. Aliquam assumenda atque deleniti eos eum expedita id inventore ipsa iste laboriosam libero magni molestias omnis, quia quis quod quos reiciendis sit sunt unde veniam veritatis voluptatem. At fugiat hic itaque magni modi, possimus quas quibusdam. Dolores eius enim fugiat fugit id ipsam sed suscipit ut! Aliquid animi atque aut corporis dicta dignissimos dolorem eius eligendi illo laudantium natus nihil placeat quibusdam quis, quo reprehenderit tempora.
+        </p>
+    </article>
+    <article>
+        <h1>
+            <a href="/post">
+                My Third Post
+            </a>
+        </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consequuntur doloremque incidunt ipsam magni nam nemo, nesciunt omnis provident soluta sunt suscipit, vitae voluptatum. Aut cupiditate debitis incidunt laborum nobis obcaecati pariatur possimus tenetur. Aliquam assumenda atque deleniti eos eum expedita id inventore ipsa iste laboriosam libero magni molestias omnis, quia quis quod quos reiciendis sit sunt unde veniam veritatis voluptatem. At fugiat hic itaque magni modi, possimus quas quibusdam. Dolores eius enim fugiat fugit id ipsam sed suscipit ut! Aliquid animi atque aut corporis dicta dignissimos dolorem eius eligendi illo laudantium natus nihil placeat quibusdam quis, quo reprehenderit tempora.
+        </p>
+    </article>
+</body>
+</html>
