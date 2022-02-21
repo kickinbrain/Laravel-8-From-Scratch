40b71ac4687f8abc91a539de90bb405cfc74e170
kickinbrain <kickinbrain@gmail.com>
2022-02-21 10:18:01 +0300

    completed episode 11 - use the Filesystem Class to Read a Directory

---
+++ app/Models/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
+class Post
+{
+    public static function find($slug)
+    {
+    if(!file_exists($path = resource_path("posts/{$slug}.html"))){
+        throw new ModelNotFoundException();
+    }
+
+        return $post = cache()->remember("post.{$slug}", now()->addDay(), fn() => file_get_contents($path));
+    }
+
+    public static function all()
+    {
+        $files = File::files(resource_path("posts/"));
+        return array_map(function($file){
+            return $file->getContents();
+        }, $files);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
  <?php
 
-use Illuminate\Support\Facades\Route;
+ use App\Models\Post;
+ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,16 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts');
+    return view('posts',[
+        'posts' => Post::all()
+    ]);
 });
 
 Route::get('posts/{post}', function ($slug){
 
-    if(!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")){
-        abort(404);
-    }
+    $post = Post::find($slug);
 
-    $post = cache()->remember("post.{$slug}", now()->addDay(), fn() => file_get_contents($path));
+    return view('post',[
+        'post' => $post
+    ]);
 
-    return view('post', ['post' => $post]);
 })->where('post', '[A-z_\-]+');
