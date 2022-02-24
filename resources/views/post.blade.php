064b798245737d454a25eea563dc7e3ad82652fb
kickinbrain <kickinbrain@gmail.com>
2022-02-24 22:38:49 +0300

    completed episode 15 - blade layouts two ways

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,22 +1,13 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>My First Post</title>
-</head>
-<body>
-    <article>
-        <h1>
-            {{ $post->title }}
-        </h1>
-        <div>
-            {!! $post->body !!}
-        </div>
-    </article>
-    <a href="/">Go Back</a>
-</body>
-</html>
+<x-layout>
+    <x-slot name="content">
+         <article>
+            <h1>
+                {{ $post->title }}
+            </h1>
+            <div>
+                {!! $post->body !!}
+            </div>
+        </article>
+        <a href="/">Go Back</a>
+    </x-slot>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,12 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./app.css">
-    <title>My blog</title>
-</head>
-<body>
-    @foreach ($posts as $post)
-        <article>
-            <a href="/posts/{{ $post->slug }}">
-                <h1>{{ $post->title }}</h1>
-            </a>
-            <div>{{ $post->expert }}</div>
-        </article>
-    @endforeach
-</body>
-</html>
+<x-layout>
+    <x-slot name="content">
+        @foreach ($posts as $post)
+            <article>
+                <a href="/posts/{{ $post->slug }}">
+                    <h1>{{ $post->title }}</h1>
+                </a>
+                <div>{{ $post->expert }}</div>
+            </article>
+        @endforeach
+    </x-slot>
+</x-layout>
