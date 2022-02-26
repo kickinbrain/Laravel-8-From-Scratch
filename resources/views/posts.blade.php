457596d36db6ce2beb972de471b45e6c12e55002
kickinbrain <kickinbrain@gmail.com>
2022-02-26 23:46:17 +0300

    completed episode 20 - Make a Post Model and Migration

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="content">
         @foreach ($posts as $post)
             <article>
-                <a href="/posts/{{ $post->slug }}">
+                <a href="/posts/{{ $post->id }}">
                     <h1>{{ $post->title }}</h1>
                 </a>
                 <div>{{ $post->expert }}</div>
