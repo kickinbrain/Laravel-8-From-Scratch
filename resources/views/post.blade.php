5738b2ba418b2b316a239c38f00cbb464d8784f6
kickinbrain <kickinbrain@gmail.com>
2022-03-06 08:34:16 +0400

    completed episode 25 - Show All Posts Associated With a Category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,8 +8,8 @@
                 {!! $post->body !!}
             </div>
              <p>
-                 <a href="{{ $post->category->slug }}">
-                     {{ $post->category->name }}
+                 <a href="categories/{{ $post->category->slug }}">
+                    {{ $post->category->name }}
                  </a>
              </p>
         </article>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
                     <h1>{!! $post->title !!}</h1>
                 </a>
                 <p>
-                    <a href="{{ $post->category->slug }}">
+                    <a href="categories/{{ $post->category->slug }}">
                         {{ $post->category->name }}
                     </a>
                 </p>
