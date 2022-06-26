17ec7fe20b11a5536c0e1d371fcd38949a9bdccd
kickinbrain <kickinbrain@gmail.com>
2022-06-26 12:55:17 +0400

    delete project

--- resources/views/components/category-button.blade.php
+++
@@ -1,7 +0,0 @@
-@props(['category'])
-
-<div>
-    <a href="/categories/{{ $category->slug }}" class="px-3 py-1 border rounded-full border-blue-300 text-blue-300 text-xs uppercase font-semibold">
-        {{ $category->name }}
-    </a>
-</div>
