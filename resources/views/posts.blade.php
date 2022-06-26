fc8c1b5610b5a86f0bac46ba9b3ca10456d0992e
kickinbrain <kickinbrain@gmail.com>
2022-06-26 17:38:28 +0400

    completed episode 7 - Make a Route and Link to it

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My blog</title>
+    <link rel="stylesheet" href="./app.css">
+</head>
+<body>
+
+<article>
+    <a href="post">
+        <h2>My first post</h2>
+    </a>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis eaque earum ipsam magni quisquam. Ea expedita inventore ipsa molestiae porro provident quam? Aliquid cum cupiditate earum id impedit incidunt labore nesciunt perferendis possimus, quae quis, quos, ratione rerum tenetur ut velit voluptate? Adipisci eaque harum molestias nulla odio reprehenderit ullam veniam! Ad aspernatur beatae, eligendi et exercitationem, fugit impedit inventore nobis placeat qui quidem ratione rerum totam, voluptates voluptatibus. A ea eligendi excepturi harum ipsam nemo perspiciatis tempore voluptatem. Molestiae!
+    </p>
+</article>
+
+<article>
+    <h2>My second post</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis eaque earum ipsam magni quisquam. Ea expedita inventore ipsa molestiae porro provident quam? Aliquid cum cupiditate earum id impedit incidunt labore nesciunt perferendis possimus, quae quis, quos, ratione rerum tenetur ut velit voluptate? Adipisci eaque harum molestias nulla odio reprehenderit ullam veniam! Ad aspernatur beatae, eligendi et exercitationem, fugit impedit inventore nobis placeat qui quidem ratione rerum totam, voluptates voluptatibus. A ea eligendi excepturi harum ipsam nemo perspiciatis tempore voluptatem. Molestiae!
+    </p>
+</article>
+
+<article>
+    <h2>My third post</h2>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis eaque earum ipsam magni quisquam. Ea expedita inventore ipsa molestiae porro provident quam? Aliquid cum cupiditate earum id impedit incidunt labore nesciunt perferendis possimus, quae quis, quos, ratione rerum tenetur ut velit voluptate? Adipisci eaque harum molestias nulla odio reprehenderit ullam veniam! Ad aspernatur beatae, eligendi et exercitationem, fugit impedit inventore nobis placeat qui quidem ratione rerum totam, voluptates voluptatibus. A ea eligendi excepturi harum ipsam nemo perspiciatis tempore voluptatem. Molestiae!
+    </p>
+</article>
+
+
+</body>
+</html>
