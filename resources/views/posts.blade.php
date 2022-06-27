19b9d3e2cdbd014b229442c4ead620f0ddbe84b7
kickinbrain <kickinbrain@gmail.com>
2022-06-27 22:44:07 +0400

    completed episode 15 - Blade Layouts Two Ways

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,28 +1,16 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My blog</title>
-    <link rel="stylesheet" href="./app.css">
-</head>
-<body>
+<x-layout>
+    <x-slot name="content">
+        @foreach ($posts as $post)
+            <article>
+                <h1>
+                    {{ $post->title }}
+                </h1>
 
-@foreach ($posts as $post)
-    <article>
-        <h1>
-            {{ $post->title }}
-        </h1>
-
-        <div>
-            {{ $post->excerpt }}
-            <a href="posts/{{ $post->slug }}">Read more</a>
-        </div>
-    </article>
-@endforeach
-
-
-</body>
-</html>
+                <div>
+                    {{ $post->excerpt }}
+                    <a href="posts/{{ $post->slug }}">Read more</a>
+                </div>
+            </article>
+        @endforeach
+    </x-slot>
+</x-layout>
