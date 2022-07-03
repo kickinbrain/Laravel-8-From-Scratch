b31896c5b7e7933b941a8f7d1c576f490bd6b27d
kickinbrain <kickinbrain@gmail.com>
2022-07-03 23:32:07 +0400

    completed episode 31 - Convert the HTML and CSS to Blade

---
+++ resources/views/components/post-card.blade.php
@@ -0,0 +1,42 @@
+<article class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+    <div class="p-4">
+        <div>
+            <img src="/images/illustration-2.png" alt="Blog Post Illustration" class="rounded-xl">
+        </div>
+        <div class="flex-1 mt-8 flex flex-col justify-between">
+            <header>
+                <div>
+                    <a href="#" class="px-3 py-1 border rounded-full border-blue-300 text-blue-300 text-xs uppercase font-semibold">Techniques</a>
+                    <a href="#" class="px-3 py-1 border rounded-full border-red-300 text-red-300 text-xs uppercase font-semibold">Updates</a>
+                </div>
+
+                <div class="mt-4">
+                    <h1 class="text-3xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
+                    <span class="mt-2 block mb-2 text-gray-400 text-xs">
+                                <time>1 day ago</time>
+                            </span>
+                </div>
+            </header>
+
+            <div class="text-sm mt-4">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, voluptatem!</p>
+                <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad architecto culpa incidunt sequi? Amet consequuntur corporis eaque sequi ut?</p>
+                <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, voluptatem!</p>
+                <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam architecto dolorem expedita iusto magni nihil.</p>
+            </div>
+
+            <footer class="flex justify-between items-center mt-8">
+                <div class="flex items-center text-sm">
+                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                    <div class="ml-3">
+                        <h5 class="font-bold">Lary Laracore</h5>
+                        <h6>Mascot at Laracasts</h6>
+                    </div>
+                </div>
+                <div>
+                    <a href="#" class="text-xs font-semibold bg-gray-300 rounded-full hidden lg:block py-2 px-4">Read more</a>
+                </div>
+            </footer>
+        </div>
+    </div>
+</article>
